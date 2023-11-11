b14d7566f27d4d972648df4e0346462a48706ce0
bartek33642 <92989898+bartek33642@users.noreply.github.com>
2023-11-11 20:44:38 +0100

    some changes - server

--- server/app/REST/subject.endpoint.js
+++ server/app/REST/subject.endpoint.js
@@ -1,16 +1,15 @@
-import { Router } from 'express';
 import { body, validationResult } from 'express-validator';
 import business from "../business/business.container";
 import { isValidObjectId } from "mongoose";
-
-const router = Router();
+const express = require('express');
+const router = express();
 
 export const subjectEndpoint = (router) => {
   // Pobierz wszystkie przedmioty
   router.get('/subjects', async (req, res) => {
     try {
-      const subjects = await business.subjectBusiness.getAllSubjects();
-      res.json(subjects);
+      const subjects = await business.getSubjectsManager().query();
+      res.status(200).send(subjects);
     } catch (error) {
       console.error('Błąd podczas pobierania przedmiotów:', error);
       res.status(500).json({ error: 'Wystąpił błąd podczas pobierania przedmiotów.' });
@@ -20,19 +19,21 @@ export const subjectEndpoint = (router) => {
   // Pobierz przedmiot o określonym ID
   router.get('/subjects/:id', async (req, res) => {
     const subjectId = req.params.id;
+    const nameSubject = req.params.name;
 
     if (!isValidObjectId(subjectId)) {
       return res.status(400).json({ error: 'Nieprawidłowy format ID przedmiotu.' });
     }
 
     try {
-      const subject = await business.subjectBusiness.getSubjectById(subjectId);
-
+      const subject = await business.getSubjectsManager(subjectId).query();
+      console.log("subjectName: ", nameSubject);
       if (!subject) {
         return res.status(404).json({ error: 'Przedmiot o podanym ID nie istnieje.' });
       }
 
-      res.json(subject);
+      // res.status(200).send(subjectId);
+      res.json(subjectId);
     } catch (error) {
       console.error('Błąd podczas pobierania przedmiotu:', error);
       res.status(500).json({ error: 'Wystąpił błąd podczas pobierania przedmiotu.' });
@@ -53,8 +54,9 @@ export const subjectEndpoint = (router) => {
     const subjectData = { name };
 
     try {
-      const createdSubject = await business.subjectBusiness.createOrUpdateSubject(subjectData);
-      res.status(201).json(createdSubject);
+      // const createdSubject = await business.subjectBusiness.createOrUpdateSubject(subjectData);
+      const createdSubject = await business.getSubjectsManager().createOrUpdate(subjectData);
+      res.status(201).send(createdSubject);
     } catch (error) {
       console.error('Błąd podczas dodawania przedmiotu:', error);
       res.status(500).json({ error: 'Wystąpił błąd podczas dodawania przedmiotu.' });
@@ -81,8 +83,10 @@ export const subjectEndpoint = (router) => {
     const subjectData = { subjectId, name };
 
     try {
-      const updatedSubject = await business.subjectBusiness.createOrUpdateSubject(subjectData);
-      res.json(updatedSubject);
+      // const updatedSubject = await business.subjectBusiness.createOrUpdateSubject(subjectData);
+      const updatedSubject = await business.getSubjectsManager().createOrUpdate(subjectData);
+      res.status(200).send(updatedSubject);
+      // res.json(updatedSubject);
     } catch (error) {
       console.error('Błąd podczas aktualizacji przedmiotu:', error);
       res.status(500).json({ error: 'Wystąpił błąd podczas aktualizacji przedmiotu.' });
@@ -98,18 +102,18 @@ export const subjectEndpoint = (router) => {
     }
 
     try {
-      const deletedSubject = await business.subjectBusiness.deleteSubject(subjectId);
+      const deletedSubject = await business.getSubjectsManager().deleteSubject(subjectId);
 
       if (!deletedSubject) {
         return res.status(404).json({ error: 'Przedmiot o podanym ID nie istnieje.' });
       }
 
-      res.json({ message: 'Przedmiot został usunięty.' });
+      // res.json({ message: 'Przedmiot został usunięty.' });
+      res.status(200).send({ message:  `Przedmiot o id: ${subjectId} został usunięty.` });
+
     } catch (error) {
       console.error('Błąd podczas usuwania przedmiotu:', error);
       res.status(500).json({ error: 'Wystąpił błąd podczas usuwania przedmiotu.' });
     }
   });
 };
-
-export default router;
